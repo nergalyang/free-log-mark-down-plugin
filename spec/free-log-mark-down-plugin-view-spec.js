4aed1646b1e729ebd55fc426e966f30541df2f21
Phil <224063001@qq.com>
2017-07-17 13:58:00 +0800

    initial commit

---
+++ spec/free-log-mark-down-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FreeLogMarkDownPluginView from '../lib/free-log-mark-down-plugin-view';
+
+describe('FreeLogMarkDownPluginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
